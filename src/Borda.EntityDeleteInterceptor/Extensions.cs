88a7e737991cefc561444e14462bac9ff9ea2f60
Emre Teoman <emre.teoman@bordatech.com>
2021-03-21 13:47:05 +0300

    summary comments included

--- src/Borda.EntityDeleteInterceptor/Extensions.cs
+++ src/Borda.EntityDeleteInterceptor/Extensions.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Borda.EntityDeleteInterceptor
@@ -9,10 +8,10 @@ namespace Borda.EntityDeleteInterceptor
     public static class Extensions
     {
         /// <summary>
-        /// 
+        /// Scans and adds <see cref="IEntityDeleteInterceptor{TEntity}"/> implementation instances to the service provider as scoped.
         /// </summary>
-        /// <param name="services"></param>
-        /// <param name="assemblies"></param>
+        /// <param name="services">to add services to.</param>
+        /// <param name="assemblies">Assemblies to scan <see cref="IEntityDeleteInterceptor{TEntity}"/> implementations.</param>
         public static void AddEntityDeleteInterceptor(this IServiceCollection services,
             params Assembly[] assemblies)
         {
@@ -28,11 +27,11 @@ namespace Borda.EntityDeleteInterceptor
         }
         
         /// <summary>
-        /// 
+        /// Adds the interceptor to handle <see cref="IEntityDeleteInterceptor{TEntity}"/> DeletingEntity method.
         /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="serviceProvider"></param>
-        /// <returns></returns>
+        /// <param name="builder">to add interceptor.</param>
+        /// <param name="serviceProvider">To get registered <see cref="IEntityDeleteInterceptor{TEntity}"/> implementations.</param>
+        /// <returns> The same builder instance so that multiple calls can be chained. </returns>
         public static DbContextOptionsBuilder AddEntityDeleteInterceptors(this DbContextOptionsBuilder builder,
             IServiceProvider serviceProvider)
         {
--- src/Borda.EntityDeleteInterceptor/IEntityDeleteInterceptor.cs
+++ src/Borda.EntityDeleteInterceptor/IEntityDeleteInterceptor.cs
@@ -1,15 +1,15 @@
 namespace Borda.EntityDeleteInterceptor
 {
     /// <summary>
-    /// 
+    /// Allows deletion interception for given entity type.
     /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TEntity">Type of entity class.</typeparam>
     public interface IEntityDeleteInterceptor<in TEntity>
     {
         /// <summary>
-        /// 
+        /// Called at the start of DbContext.SaveChanges for each entity if entity state is Deleted.
         /// </summary>
-        /// <param name="entity"></param>
+        /// <param name="entity">Tracked entity instance.</param>
         void DeletingEntity(TEntity entity);
     }
 }
